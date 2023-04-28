8fe994611db829c219d42789d1ae6a5d99a9800f
mtchwin <mitchellswinn@gmail.com>
2023-04-28 13:31:24 -0700

    movement fix

--- Scripts/CharMain.gd
+++ Scripts/CharMain.gd
@@ -14,19 +14,19 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _input(event):
 
-	if event.is_action("left"):
+	if event.is_action_pressed("left"):
 		left = true
 	elif event.is_action_released("left"):
 		left = false
-	if event.is_action("right"):
+	if event.is_action_pressed("right"):
 		right = true
 	elif event.is_action_released("right"):
 		right = false
-	if event.is_action("up"):
+	if event.is_action_pressed("up"):
 		up = true
 	elif event.is_action_released("up"):
 		up = false
-	if event.is_action("down"):
+	if event.is_action_pressed("down"):
 		down = true
 	elif event.is_action_released("down"):
 		down = false
@@ -36,12 +36,12 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	if left:
-		velocity.x = -144
-	elif right:
-		velocity.x = 144
-	elif up:
-		velocity.y = -144
-	elif down:
-		velocity.y = 144
+		velocity.x += -144
+	if right:
+		velocity.x += 144
+	if up:
+		velocity.y += -144
+	if down:
+		velocity.y += 144
 
 	move_and_slide()
